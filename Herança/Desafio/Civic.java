8ee7a2fead0fe96e5d47732950c54b2eed00fac0
Vitor Scholze <vitorscholze7@gmail.com>
2026-03-18 12:10:29 -0300

    Commit Exericios

---
+++ Herança/Desafio/Civic.java
@@ -0,0 +1,8 @@
+package Desafio;
+
+public class Civic extends Carro {
+    Civic(){
+        super(220);
+    }
+
+}
---
+++ Herança/Desafio/Ferrari.java
@@ -0,0 +1,16 @@
+package Desafio;
+
+public class Ferrari extends Carro {
+
+    Ferrari(){
+        super(300);
+        delta = 15;
+    }
+    
+    // void acelerar(){
+    //this.velocidade += 15;
+    //}
+
+
+
+}
