7c5a08670d60846b812d931d8ace24a9c46b0779
Vitor Scholze <vitorscholze7@gmail.com>
2026-03-25 13:42:31 -0300

    Commit exrcicios

---
+++ Lambdas/Binary Operator e BiFunction/OperadorBinario.java
@@ -0,0 +1,20 @@
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class OperadorBinario {
+    public static void main(String[] args) {
+        
+        BinaryOperator<Double> media = (n1, n2) -> (n1 + n2) / 2;
+        System.out.println(media.apply(9.8, 5.7));
+
+        BiFunction<Double, Double, String> resultado = (n1, n2) -> ((n1 + n2) / 2) >=7 ? "Aprovado " : "Reprovado";
+
+        System.out.println(resultado.apply(9.7, 4.1));
+
+        Function<Double, String> resultado2 = m -> m >=7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(media.andThen(resultado2).apply(9.7, 5.1));
+
+    }
+}
