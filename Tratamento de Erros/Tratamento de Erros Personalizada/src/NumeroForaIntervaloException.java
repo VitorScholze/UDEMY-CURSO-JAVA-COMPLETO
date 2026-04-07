d94e1f59e9baabba8d003610b2477b1f68d8c0b0
Vitor Scholze <vitorscholze7@gmail.com>
2026-04-07 12:41:34 -0300

    Commit Exercicios

---
+++ Tratamento de Erros/Tratamento de Erros Personalizada/src/NumeroForaIntervaloException.java
@@ -0,0 +1,16 @@
+public class NumeroForaIntervaloException extends RuntimeException{
+    
+    private String nomeDoAtributo;
+
+    public NumeroForaIntervaloException(String nomeDoAtributo){
+        this.nomeDoAtributo = nomeDoAtributo;
+    }   
+
+    public String getMessage(){
+        return String.format("O atributo %s está fora do intervalo", nomeDoAtributo);
+    }
+
+
+
+
+}
