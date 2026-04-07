ac386a487b668cbd782f3585a6e136a70abe5415
Vitor Scholze <vitorscholze7@gmail.com>
2026-04-07 12:40:27 -0300

    Commit Execicios

---
+++ Tratamento de Erros/Excecao/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Aluno a1 = null;
+
+        try{
+            imprimirNomeDoAluno(a1);
+        }catch(Exception excecao) {
+            System.out.println("Ocorreu um erro no momento de imprimir o nome do usuario");
+        }
+
+        try {
+            System.out.println( 7 / 0);
+        }catch(ArithmeticException e){
+            System.out.println("Ocorreu o erro " + e.getMessage());;
+        }
+
+        System.out.println("Fim");
+    }
+
+    public static void imprimirNomeDoAluno (Aluno aluno){
+        System.out.println(aluno.nome);
+    }
+}
