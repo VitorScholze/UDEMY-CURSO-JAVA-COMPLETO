36349f9c051032aa330bfb964cb8ed7938152403
Vitor Scholze <vitorscholze7@gmail.com>
2026-05-22 19:13:23 -0300

    commit composicao exercicios(Embeddable)

---
+++ ExerciciosJPA/estudos-jpa/jpa-estudos/src/main/java/com/vitor/modelo/composicao/Fornecedor.java
@@ -0,0 +1,50 @@
+package com.vitor.modelo.composicao;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "fonecedores")
+public class Fornecedor {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private Endereco endereco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+
+    
+}
+
+
