801dd16f31ed120a0c842b321477a8617a620a64
Vitor Scholze <vitorscholze7@gmail.com>
2026-05-22 19:11:33 -0300

    commit exercicios JPA heranca

---
+++ ExerciciosJPA/estudos-jpa/jpa-estudos/src/main/java/com/vitor/modelo/heranca/Aluno.java
@@ -0,0 +1,50 @@
+package com.vitor.modelo.heranca;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo", length = 2 , discriminatorType = DiscriminatorType.STRING)
+@DiscriminatorValue("AL")
+public class Aluno {
+    
+    @Id
+    private Long matricula;
+
+    private String nome;
+
+    public Aluno(){
+
+    }
+
+    public Aluno(Long matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+
+    
+}
