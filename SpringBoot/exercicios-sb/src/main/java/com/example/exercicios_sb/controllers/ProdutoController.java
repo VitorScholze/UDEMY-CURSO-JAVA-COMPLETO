8f900a443f78eb21e871bdefc750a4c8af7121f8
Vitor Scholze <vitorscholze7@gmail.com>
2026-06-01 22:24:34 -0300

    commit controllerProduto

---
+++ SpringBoot/exercicios-sb/src/main/java/com/example/exercicios_sb/controllers/ProdutoController.java
@@ -0,0 +1,75 @@
+package com.example.exercicios_sb.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.exercicios_sb.models.entities.Produto;
+import com.example.exercicios_sb.models.entities.repositories.ProdutoRepository;
+
+import jakarta.validation.Valid;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+    public Produto salvarProduto(@RequestBody @Valid Produto produto){ //@Valid vai conferir todas as avaliacoes das anotations
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    @GetMapping
+    public Iterable<Produto> obterProdutos(){
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
+    public Iterable<Produto> obterProdutoPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdePagina){
+        if(qtdePagina >=5) qtdePagina = 5;
+        Pageable page = PageRequest.of(numeroPagina,qtdePagina);
+        return produtoRepository.findAll(page);
+    }
+
+    @GetMapping(path = "/nome/{nome}")
+    public Iterable<Produto> obterProdutoPorNome(@PathVariable String nome){
+        return produtoRepository.findByNomeContainingIgnoreCase(nome);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Produto> obterProdutoPorId(@PathVariable int id){
+       return produtoRepository.findById(id);
+    }
+
+    //@PutMapping
+    //public Produto alterarProduto(@Valid Produto p){
+    //    produtoRepository.save(p);
+    //    return p;
+    //}
+
+    @DeleteMapping(path = "/{id}")
+    public void excluirProduto(@PathVariable int id){
+        produtoRepository.deleteById(id);
+        
+    }
+
+
+    
+}
+
